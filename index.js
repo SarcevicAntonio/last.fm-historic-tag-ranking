895c969c46cf5b94040f9a799bbc3151ac861a6b
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-07-24 16:24:04 +0200

    make it with code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { import_data, import_tag_per_artist } from './import.js'
 
+const IGNORED_TAGS = [null, 'seen live', 'musik um sich allein zu betrinken', 'dank']
+
 async function main() {
 	let data = await import_data()
 	const tag_per_artist = import_tag_per_artist()
@@ -10,7 +12,7 @@ async function main() {
 		entry.year = entry.date.getFullYear()
 	}
 
-	const tag_count_each_year = {}
+	const tag_count_per_year = {}
 
 	for (let i = 0; i < data.length; i++) {
 		const scrobbl = data[i]
@@ -18,20 +20,39 @@ async function main() {
 			continue
 		}
 		let tag = tag_per_artist.get(scrobbl.artist)
-		if (!tag_count_each_year[tag]) tag_count_each_year[tag] = {}
-		if (!tag_count_each_year[tag][scrobbl.year]) tag_count_each_year[tag][scrobbl.year] = 0
-		tag_count_each_year[tag][scrobbl.year]++
+		if (IGNORED_TAGS.includes(tag)) continue
+		if (!tag_count_per_year[scrobbl.year]) tag_count_per_year[scrobbl.year] = []
+		let tag_in_year = tag_count_per_year[scrobbl.year].find((i) => i.tag === tag)
+		if (!tag_in_year) {
+			tag_in_year = { tag, count: 0 }
+			tag_count_per_year[scrobbl.year].push(tag_in_year)
+		}
+		tag_in_year.count++
+	}
+
+	for (const array of Object.values(tag_count_per_year)) {
+		array.sort((a, b) => b.count - a.count)
+		array.splice(30, array.length - 1)
+	}
+
+	const tag_set = new Set()
+	for (const array of Object.values(tag_count_per_year)) {
+		array.forEach(({ tag }) => {
+			tag_set.add(tag)
+		})
 	}
 
 	console.log('tag,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023')
-	for (const [tag, years_count] of Object.entries(tag_count_each_year)) {
-		let row = [tag]
-
-		for (const [year, count] of Object.entries(years_count)) {
-			row[year - 2010] = count
+	for (const tag of [...tag_set]) {
+		const row = [tag]
+		for (let year = 2011; year <= 2023; year++) {
+			const index = tag_count_per_year[year].findIndex((i) => i.tag === tag)
+			if (index === -1) {
+				row.push('')
+			} else {
+				row.push(index + 1)
+			}
 		}
-
-		row = Array.from(row, (v) => (v ? v : 0))
 		console.log(row.map((v) => `"${v}"`).join(','))
 	}
 }
